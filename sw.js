b1f69a398eb134debf1768b9e1c5ec953f7573d6
Yazu5 <adriansuarez0509@gmail.com>
2025-12-25 02:42:08 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  console.log('Yapper Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Keeps the app running smoothly
+});
